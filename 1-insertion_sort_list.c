b730877df28bf66c5e0b39af2ad12827d40fe63a
PeterEkwere <peterekwere04@gmail.com>
2023-07-21 12:21:17 -0700

    created task 1 file

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - This function implements
+ * 	The insertion algo on a linked list
+ *
+ * @list: is a double pointer to the first node
+ * Return: void
+ */
+void insertions_sort_list(listint_t **list)
+{
+	listint_t *temp = list
+}
