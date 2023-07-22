d0949c7fe7979d4d9f4fef59e8eb4c27f3954796
sapera11 <akinsetewilliams@gmail.com>
2023-07-22 07:23:11 -0700

    updated

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,50 +5,59 @@ void swap(int *a, int *b);
 void print_array(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
-void print_array(int *array, size_t size) {
-    for (size_t i = 0; i < size; i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
+void print_array(int *array, size_t size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("%d ", array[i]);
+	}
+		printf("\n");
 }
 
-void bubble_sort(int *array, size_t size) {
-    size_t i, len = size;
-    int bubbly = 0;
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len =
+	int bubbly = 0;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    while (bubbly == 0) {
-        bubbly = 1;
-        for (i = 0; i < len - 1; i++) {
-            if (array[i] > array[i + 1]) {
-                swap(&array[i], &array[i + 1]);
-                print_array(array, size);
-                bubbly = 0;
-            }
-        }
-        len--;
-    }
+	while (bubbly == 0)
+	{
+	bubbly = 1;
+	for (i = 0; i < len - 1; i++)
+	{
+	if (array[i] > array[i + 1])
+	{
+		swap(&array[i], &array[i + 1]);
+		print_array(array, size);
+		bubbly = 0;
+	}
+	}
+	len--;
+	}
+	{
 }
 
-int main() {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    size_t size = sizeof(arr) / sizeof(arr[0]);
+int main(void)
+{
+	int arr[] = {64, 34, 25, 12, 22, 11, 90};
+	size_t size = sizeof(arr) / sizeof(arr[0]);
 
-    printf("Original array: ");
-    print_array(arr, size);
+	printf("Original array: ");
+	print_array(arr, size);
 
-    bubble_sort(arr, size);
+	bubble_sort(arr, size);
 
-    printf("Sorted array: ");
-    print_array(arr, size);
+	printf("Sorted array: ");
+	print_array(arr, size);
 
-    return 0;
+	return (0);
 }
