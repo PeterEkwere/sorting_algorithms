f80c24c2106b75dc9b9bc723106ba69ec2164325
sapera11 <akinsetewilliams@gmail.com>
2023-07-22 12:56:06 -0700

    updated

--- sort.h
+++ sort.h
@@ -20,5 +20,22 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Printing helper functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
-#endif
+/* Sorting algorithms */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sore_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void redix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+#endif /* SORT_H */
